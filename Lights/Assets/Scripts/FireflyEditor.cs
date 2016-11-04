edc1a8cb8267fbeba1e70db77554018abbb0d6af
HenrySmithUCI <hgsmith@uci.edu>
2016-11-04 03:31:24 -0700

    Added builds

--- Lights/Assets/Scripts/FireflyEditor.cs
+++ Lights/Assets/Scripts/FireflyEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
@@ -16,3 +17,4 @@ public class FireflyEditor : Editor {
     }
   }
 }
+#endif
